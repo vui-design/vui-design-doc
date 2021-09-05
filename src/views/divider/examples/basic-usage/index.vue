e1185584c690b371be7cbc5adc82bd89fc8182b3
vui-design <vui.design@aliyun.com>
2021-09-05 21:06:26 +0800

    [improve]优化部分组件及其样式

--- src/views/divider/examples/basic-usage/index.vue
+++ src/views/divider/examples/basic-usage/index.vue
@@ -1,33 +1,33 @@
 <template>
-	<example v-bind:code="code" jsFiddle="3axb104s" id="example-divider-basic-usage">
-		<template slot="demo">
-			<div class="example-divider-basic-usage">
-				<p>Be careful how you judge people, most of all friends. You don’t sum up a man’s life in one moment.</p>
-				<vui-divider />
-				<p>That was my way of getting through difficult times of low confidence – hard work.</p>
-				<vui-divider dashed />
-				<p>There is not a heart but has its moments of longing, yearning for something better, nobler and holier than it knows before.</p>
-			</div>
-		</template>
-		<template slot="title">基本用法</template>
-		<template slot="description">
-			<p>基本用法，支持虚线样式。</p>
-		</template>
-	</example>
+  <example v-bind:code="code" id="example-divider-basic-usage">
+    <template slot="demo">
+      <div class="example-divider-basic-usage">
+        <p>Be careful how you judge people, most of all friends. You don’t sum up a man’s life in one moment.</p>
+        <vui-divider />
+        <p>That was my way of getting through difficult times of low confidence – hard work.</p>
+        <vui-divider dashed />
+        <p>There is not a heart but has its moments of longing, yearning for something better, nobler and holier than it knows before.</p>
+      </div>
+    </template>
+    <template slot="title">基本用法</template>
+    <template slot="description">
+      <p>基本用法，支持虚线样式。</p>
+    </template>
+  </example>
 </template>
 
 <script>
-	import Example from "src/components/example";
-	import code from "./code";
+  import Example from "src/components/example";
+  import code from "./code";
 
-	export default {
-		components: {
-			Example
-		},
-		data() {
-			return {
-				code
-			};
-		}
-	};
+  export default {
+    components: {
+      Example
+    },
+    data() {
+      return {
+        code
+      };
+    }
+  };
 </script>
--- src/views/divider/examples/vertical/index.vue
+++ src/views/divider/examples/vertical/index.vue
@@ -1,33 +1,33 @@
 <template>
-	<example v-bind:code="code" jsFiddle="Lqbejuo5" id="example-divider-vertical">
-		<template slot="demo">
-			<div class="example-divider-vertical">
-				Text
-				<vui-divider type="vertical" />
-				<a href="javascript:;">Link</a>
-				<vui-divider type="vertical" />
-				<a href="javascript:;">Link</a>
-			</div>
-		</template>
-		<template slot="title">垂直方向</template>
-		<template slot="description">
-			<p>使用 <code>type="vertical"</code> 设置行内垂直方向的分割线。</p>
-		</template>
-	</example>
+  <example v-bind:code="code" id="example-divider-vertical">
+    <template slot="demo">
+      <div class="example-divider-vertical">
+        Text
+        <vui-divider type="vertical" />
+        <a href="javascript:;">Link</a>
+        <vui-divider type="vertical" />
+        <a href="javascript:;">Link</a>
+      </div>
+    </template>
+    <template slot="title">垂直方向</template>
+    <template slot="description">
+      <p>使用 <code>type="vertical"</code> 设置行内垂直方向的分割线。</p>
+    </template>
+  </example>
 </template>
 
 <script>
-	import Example from "src/components/example";
-	import code from "./code";
+  import Example from "src/components/example";
+  import code from "./code";
 
-	export default {
-		components: {
-			Example
-		},
-		data() {
-			return {
-				code
-			};
-		}
-	};
+  export default {
+    components: {
+      Example
+    },
+    data() {
+      return {
+        code
+      };
+    }
+  };
 </script>
