c0a6301d9ed120125d5a3186b08057df44fef768
vui-design <vui.design@aliyun.com>
2021-12-30 22:07:12 +0800

    [update]日常维护

--- src/views/dropdown/examples/cascaded/index.vue
+++ src/views/dropdown/examples/cascaded/index.vue
@@ -5,12 +5,18 @@
         <a href="javascript:;">Hover me</a>
         <vui-dropdown-menu slot="menu" v-on:click="handleClick">
           <vui-dropdown-menu-item value="1" icon="settings" title="Menu Item 1" />
-          <vui-dropdown-menu-item value="2" icon="home" title="Menu Item 2" />
-          <vui-dropdown-menu-item disabled value="3" icon="checkmark" title="Menu Item 3" />
-          <vui-dropdown-submenu value="4" icon="search" title="Menu Item 4">
-            <vui-dropdown-menu-item value="4-1" title="Menu Item 4-1" />
-            <vui-dropdown-menu-item value="4-2" title="Menu Item 4-2" />
-            <vui-dropdown-menu-item value="4-3" title="Menu Item 4-3" />
+          <vui-dropdown-menu-item disabled value="2" icon="checkmark" title="Menu Item 2" />
+          <vui-dropdown-menu-divider />
+          <vui-dropdown-menu-item danger value="3" icon="home" title="Menu Item 3" />
+          <vui-dropdown-menu-item danger disabled value="4" icon="home" title="Menu Item 4" />
+          <vui-dropdown-menu-item-group title="Group">
+            <vui-dropdown-menu-item value="5" icon="settings" title="Menu Item 5" />
+            <vui-dropdown-menu-item value="6" icon="home" title="Menu Item 6" />
+          </vui-dropdown-menu-item-group>
+          <vui-dropdown-submenu value="7" icon="search" title="Menu Item 7">
+            <vui-dropdown-menu-item value="7-1" title="Menu Item 7-1" />
+            <vui-dropdown-menu-item value="7-2" title="Menu Item 7-2" />
+            <vui-dropdown-menu-item value="7-3" title="Menu Item 7-3" />
           </vui-dropdown-submenu>
         </vui-dropdown-menu>
       </vui-dropdown>
@@ -18,12 +24,18 @@
         <a href="javascript:;">Hover me</a>
         <vui-dropdown-menu slot="menu" color="dark" v-on:click="handleClick">
           <vui-dropdown-menu-item value="1" icon="settings" title="Menu Item 1" />
-          <vui-dropdown-menu-item value="2" icon="home" title="Menu Item 2" />
-          <vui-dropdown-menu-item disabled value="3" icon="checkmark" title="Menu Item 3" />
-          <vui-dropdown-submenu value="4" icon="search" title="Menu Item 4">
-            <vui-dropdown-menu-item value="4-1" title="Menu Item 4-1" />
-            <vui-dropdown-menu-item value="4-2" title="Menu Item 4-2" />
-            <vui-dropdown-menu-item value="4-3" title="Menu Item 4-3" />
+          <vui-dropdown-menu-item disabled value="2" icon="checkmark" title="Menu Item 2" />
+          <vui-dropdown-menu-divider />
+          <vui-dropdown-menu-item danger value="3" icon="home" title="Menu Item 3" />
+          <vui-dropdown-menu-item danger disabled value="4" icon="home" title="Menu Item 4" />
+          <vui-dropdown-menu-item-group title="Group">
+            <vui-dropdown-menu-item value="5" icon="settings" title="Menu Item 5" />
+            <vui-dropdown-menu-item value="6" icon="home" title="Menu Item 6" />
+          </vui-dropdown-menu-item-group>
+          <vui-dropdown-submenu value="7" icon="search" title="Menu Item 7">
+            <vui-dropdown-menu-item value="7-1" title="Menu Item 7-1" />
+            <vui-dropdown-menu-item value="7-2" title="Menu Item 7-2" />
+            <vui-dropdown-menu-item value="7-3" title="Menu Item 7-3" />
           </vui-dropdown-submenu>
         </vui-dropdown-menu>
       </vui-dropdown>
